ac23f23c2d911756dc4903bad8b0b7a397014286
Sokphol <sokphol123@gmail.com>
2024-09-13 15:51:08 +0700

    hi

--- build/ecosystem.config.js
+++ build/ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "product-service",
-      script: "server.js",
+      name: "service",
+      script: "./server.js",
       instances: 1,
       autorestart: true,
       watch: false,
